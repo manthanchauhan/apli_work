f1566b72cd7b26d98ace4b79a41251822323ef51
Priyam <priyam.ds@somaiya.edu>
2019-04-06 01:19:04 +0530

    jobs page working

--- apliai/recruiter/urls.py
+++ apliai/recruiter/urls.py
@@ -2,4 +2,9 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('dashboard', views.dashboard , name="dashboard"),
+    path('jobs', views.jobs , name="jobs"),
+    path('candidates', views.candidates , name="candidates"),
+    path('team', views.candidates , name="team"),
+    path('feedback', views.candidates , name="feedback"),
+    path('question', views.candidates , name="question"),        
 ]
--- apliai/recruiter/views.py
+++ apliai/recruiter/views.py
@@ -7,3 +7,18 @@ def dashboard(request):
     return render(request,'recruiter/dashboard.html')
 
     # return HttpResponse("Dashboard page working !")
+
+def jobs(request):
+    return render(request,'recruiter/jobs.html')
+
+def candidates(request):
+    return render(request,'recruiter/candidates.html')
+
+def team(request):
+    return render(request,'recruiter/teamhtml')
+
+def question(request):
+    return render(request,'recruiter/question.html')
+
+def feedback(request):
+    return render(request,'recruiter/feedback.html')
