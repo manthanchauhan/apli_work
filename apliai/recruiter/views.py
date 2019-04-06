f2c4c6d337097c64289b620b9c65b3caacdb486d
Priyam <priyam.ds@somaiya.edu>
2019-04-07 01:28:28 +0530

    data at backend

--- apliai/recruiter/views.py
+++ apliai/recruiter/views.py
@@ -11,11 +11,25 @@ def dashboard(request):
 def jobs(request):
     return render(request,'recruiter/jobs.html')
 
+def postjob(request):
+    if request.method == "POST":
+        company_name = "Apli.ai"
+        post = request.POST.get('post')
+        job_description = request.POST.get('jobdesc')
+        tskill = request.POST.get('tskill')
+        sskill = request.POST.get('sskill')
+        other = request.POST.get('other')
+        bond = request.POST.get('bond')
+        salary = request.POST.get('salary')
+        add_detail = request.POST.get('adddetail')
+    print(company_name,post,job_description,tskill,sskill,other,bond,salary,add_detail)
+    return render(request,"recruiter/jobs.html")
+
 def candidates(request):
     return render(request,'recruiter/candidates.html')
 
 def team(request):
-    return render(request,'recruiter/teamhtml')
+    return render(request,'recruiter/team.html')
 
 def question(request):
     return render(request,'recruiter/question.html')
