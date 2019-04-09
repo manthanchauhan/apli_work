dad3aae889920bfc539d3ef59473bd038383dfd7
Priyam <priyam.ds@somaiya.edu>
2019-04-09 23:40:05 +0530

    all form data at backend from accounts

--- apliai/accounts/views.py
+++ apliai/accounts/views.py
@@ -15,18 +15,36 @@ def reachus(request):
     return render(request,'accounts/reachus.html')
 
 def login(request):
+    if request.method == "POST":
+        email=request.POST.get('inputEmail')
+        password=request.POST.get('inputPassword')
+        print(email,password)
     return render(request,'accounts/login.html')
 
 def signup(request):
+    if request.method == "POST":
+        email=request.POST.get('inputEmail')
+        password=request.POST.get('inputPassword')
+        print(email,password)    
     return render(request,'accounts/signup.html')
 
 def step1(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        print(name)    
     return render(request,'accounts/step1.html')
 
 def step2(request):
+    if request.method == "POST":
+        cname=request.POST.get('cname')
+        print(cname)    
+
     return render(request,'accounts/step2.html')
 
 def step3(request):
+    if request.method == "POST":
+        role=request.POST.get('inlineRadioOptions')
+        print(role)    
     return render(request,'accounts/step3.html')
 
 def logout(request):
