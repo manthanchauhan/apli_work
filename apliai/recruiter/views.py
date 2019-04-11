57b7e08031a80d90215fd68a3375192581c8c229
Priyam <priyam.ds@somaiya.edu>
2019-04-11 20:24:37 +0530

    jobs data on database

--- apliai/recruiter/views.py
+++ apliai/recruiter/views.py
@@ -44,28 +44,29 @@ def jobs(request):
                     company_name = request.session['cname']
                     post = request.POST.get('post')
                     job_description = request.POST.get('jobdesc')
-                    tskill = request.POST.get('tskill').split()
-                    sskill = request.POST.get('sskill').split()
-                    other = request.POST.get('other').split()
+                    tskill = request.POST.getlist('tskill')
+                    sskill = request.POST.getlist('sskill')
+                    other = request.POST.getlist('other')
                     bond = request.POST.get('bond')
                     salary = request.POST.get('salary')
                     add_detail = request.POST.get('adddetail')
+                    
                     status = 'Opened'
-                    jobid = request.session['email']+'$'+post+'$'+datetime.now().strftime("%H:%M:%S")
-                    print(company_name,post,job_description,tskill,sskill,other,bond,salary,add_detail,status,jobid)
-                    # doc_ref = db.collection(u'jobs').document(jobid)
-                    # doc_ref.set({
-                    #     u'post': post,
-                    #     u'job_description': job_description,
-                    #     u'tskill':tskill,
-                    #     u'sskill':sskill,
-                    #     u'other':other,
-                    #     u'bond':bond,
-                    #     u'salary':salary,
-                    #     u'add_detail':add_detail,
-                    #     u'status':status,
-                    #     u'email':request.session['email']
-                    # })            
+                    jobid = request.session['email']+'$'+post+'$'+datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    # print(company_name,post,job_description,tskill,sskill,other,bond,salary,add_detail,status,jobid)
+                    doc_ref = db.collection(u'jobs').document(jobid)
+                    doc_ref.set({
+                        u'post': post,
+                        u'job_description': job_description,
+                        u'tskill':tskill,
+                        u'sskill':sskill,
+                        u'other':other,
+                        u'bond':bond,
+                        u'salary':salary,
+                        u'add_detail':add_detail,
+                        u'status':status,
+                        u'email':request.session['email']
+                    })            
                     messages.success(request, 'Job posted successfully.')
                 except:
                     messages.error(request, 'Something went wrong! Try Again Later.')    
