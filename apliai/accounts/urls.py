07599f791127dc27ca91ec9c11ec1dfe6a292d7f
shubhamchhimpa <chhimpa.shubh04@gmail.com>
2019-06-07 13:44:05 +0530

    added db logic to team signup

--- apliai/accounts/urls.py
+++ apliai/accounts/urls.py
@@ -1,18 +1,19 @@
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 from . import views
-app_name='accounts'
+
+app_name = 'accounts'
 urlpatterns = [
-    re_path(r'^reachus', views.reachus , name='reachus'),
-    re_path(r'^login', views.login , name='login'),
-    re_path(r'^signup', views.signup , name='signup'),
-    re_path(r'^step1', views.step1 , name='step1'),
-    re_path(r'^step2', views.step2 , name='step2'),
-    re_path(r'^step3', views.step3 , name='step3'),
-    path('teamsignup/<encodeddata>/', views.teamsignup , name='teamsignup'),
-    path('teamsignupcomplete/', views.teamsignupcomplete , name='teamsignupcomplete'),           
-    re_path(r'^logout', views.logout , name='logout'),
-    re_path(r'^forgot_password', views.forgot_password , name='forgot_password'),
-    path('reset_confirm/<umail>/',views.reset_confirm,name='reset_confirm'),
-    path('reset_password_successful',views.reset_password_successful,name='reset_password_successful')
+    re_path(r'^reachus', views.reachus, name='reachus'),
+    re_path(r'^login', views.login, name='login'),
+    re_path(r'^signup', views.signup, name='signup'),
+    re_path(r'^step1', views.step1, name='step1'),
+    re_path(r'^step2', views.step2, name='step2'),
+    re_path(r'^step3', views.step3, name='step3'),
+    path('teamsignup/<encodeddata>/', views.teamsignup, name='teamsignup'),
+    path('teamsignupcomplete/', views.teamsignupcomplete, name='teamsignupcomplete'),
+    re_path(r'^logout', views.logout, name='logout'),
+    re_path(r'^forgot_password', views.forgot_password, name='forgot_password'),
+    path('reset_confirm/<umail>/', views.reset_confirm, name='reset_confirm'),
+    path('reset_password_successful', views.reset_password_successful, name='reset_password_successful')
 
 ]
